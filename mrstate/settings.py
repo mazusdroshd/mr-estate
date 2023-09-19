a0e06d2540354a1251ef14b16b2caf758d7f7fe8
amirmtin <amirmatinrousta@gmail.com>
2023-09-19 16:56:46 +0330

    setup redis as cache system

--- mrstate/settings.py
+++ mrstate/settings.py
@@ -126,3 +126,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'account.CustomUser'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
