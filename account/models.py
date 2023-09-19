eebda88f6d75200c6dfb45cbabeea80024270a55
amirmtin <amirmatinrousta@gmail.com>
2023-09-19 16:46:39 +0330

    created user model and added to settings

--- account/models.py
+++ account/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from account.validators import phone_validator
+from account.managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(
+        max_length=11, unique=True, validators=[phone_validator,]
+    )
+    address = models.TextField(blank=False)
+    username = None
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = ["first_name", "last_name", "address",]
+
+    def __str__(self) -> str:
+        return self.get_full_name()
