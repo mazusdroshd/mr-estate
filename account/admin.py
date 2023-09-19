0cde878c046757a1866541c2bfe8a82f2f4eaa69
amirmtin <amirmatinrousta@gmail.com>
2023-09-19 16:48:19 +0330

    implemented admin interface for custom user

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from account.models import CustomUser
+
+
+class UserAdmin(BaseUserAdmin):
+    model = CustomUser
+    list_display = [
+        'phone_number',
+        'first_name',
+        'last_name',
+        'is_superuser'
+    ]
+    fieldsets = (
+        (None, {"fields": ("phone_number", "password")}),
+        ("Personal Info", {"fields": ("first_name", "last_name", "address")}),
+        ("Permissions", {"fields": ("is_active", "is_staff",
+         "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("phone_number", "password1", "password2", "first_name", "last_name", "address", "is_staff")}
+         ),
+    )
+    search_fields = ["phone_number"]
+    ordering = ["phone_number"]
+
+
+admin.site.register(CustomUser, UserAdmin)
