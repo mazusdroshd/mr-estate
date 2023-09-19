23aea87fe7372c6f4235781bab234fc78338c49d
amirmtin <amirmatinrousta@gmail.com>
2023-09-19 19:21:21 +0330

    set firstname and lastname as required in custom user model

--- account/models.py
+++ account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 from account.validators import phone_validator
 from account.managers import CustomUserManager
@@ -9,13 +10,14 @@ class CustomUser(AbstractUser):
     phone_number = models.CharField(
         max_length=11, unique=True, validators=[phone_validator,]
     )
+    first_name = models.CharField(_("first name"), max_length=150, blank=False)
+    last_name = models.CharField(_("last name"), max_length=150, blank=False)
     address = models.TextField(blank=False)
     username = None
 
     objects = CustomUserManager()
 
     USERNAME_FIELD = "phone_number"
-    REQUIRED_FIELDS = ["first_name", "last_name", "address",]
 
     def __str__(self) -> str:
         return self.get_full_name()
