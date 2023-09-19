3b11eadc82eede64e81ca723b79e1ef234eaa040
amirmtin <amirmatinrousta@gmail.com>
2023-09-19 19:32:51 +0330

    added jwt support for authentication

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from account import views
 
@@ -8,4 +12,6 @@ app_name = 'account'
 urlpatterns = [
     path('request_otp/', views.RequestOtpView.as_view(), name='request_otp'),
     path('signup/', views.CreateUserView.as_view(), name='signup'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
