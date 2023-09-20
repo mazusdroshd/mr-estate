9398814524404bf1ef062b3fb07c93c634e97745
amirmtin <amirmatinrousta@gmail.com>
2023-09-20 13:32:21 +0330

    added urls for media files

--- mrstate/urls.py
+++ mrstate/urls.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls', namespace='account')),
     path('advertise/', include('advertise.urls', namespace='advertise')),
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('swagger-ui/',
-         SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
+
+if settings.DEBUG:
+    urlpatterns.extend([
+        path('schema/', SpectacularAPIView.as_view(), name='schema'),
+        path('swagger-ui/',
+             SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    ])
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
