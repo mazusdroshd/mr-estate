e359cdb5b1f89c9838f31e23b4022c863ab8432d
amirmtin <amirmatinrousta@gmail.com>
2023-09-20 13:38:58 +0330

    applied filters to ads list view

--- advertise/views.py
+++ advertise/views.py
@@ -1,9 +1,15 @@
+import django_filters
+
 from rest_framework import generics
 
 from advertise.models import Advertise
 from advertise.serializers import AdvertiseListSerializer
+from advertise.filters import AdvertiseFilter
 
 
 class AdvertiseListView(generics.ListAPIView):
     serializer_class = AdvertiseListSerializer
     queryset = Advertise.objects.all()
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_class = AdvertiseFilter
+    ordering_fields = ['created', 'price']
