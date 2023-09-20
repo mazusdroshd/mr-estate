e71000a9d143b727c45a182bc224d26f9890cf35
amirmtin <amirmatinrousta@gmail.com>
2023-09-20 11:03:33 +0330

    implemented admin interface for advertises

--- advertise/admin.py
+++ advertise/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from advertise.models import Advertise, AdvertiseImage
+
+
+class AdvertiseImageInline(admin.TabularInline):
+    model = AdvertiseImage
+
+
+@admin.register(Advertise)
+class AdvertiseAdmin(admin.ModelAdmin):
+    inlines = [AdvertiseImageInline,]
+    list_display = ['title', 'price', 'user', 'created']
+    list_filter = ['price', 'created']
+    ordering = ['created', 'price']
+
+
+@admin.register(AdvertiseImage)
+class AdvertiseImageAdmin(admin.ModelAdmin):
+    list_display = ['advertise', 'image']
