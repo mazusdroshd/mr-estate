475170f09715b6cb645f94436dc4502959621cdd
amirmtin <amirmatinrousta@gmail.com>
2023-09-21 14:27:58 +0330

    removed first_name, last_name and address from user manager

--- account/managers.py
+++ account/managers.py
@@ -2,14 +2,12 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, phone_number: str, first_name: str, last_name: str, address: str, password: str = None):
+    def create_user(self, phone_number: str, address: str = None, password: str = None):
         if not phone_number:
             raise ValueError("Users must have a phone number")
 
         user = self.model(
             phone_number=phone_number,
-            first_name=first_name,
-            last_name=last_name,
             address=address,
         )
 
@@ -17,15 +15,14 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, phone_number: str, first_name: str, last_name: str, address: str, password: str = None):
+    def create_superuser(self, phone_number: str, address: str = None, password: str = None, email: str = None):
         user = self.create_user(
             phone_number=phone_number,
             password=password,
-            first_name=first_name,
-            last_name=last_name,
             address=address,
         )
         user.is_staff = True
         user.is_superuser = True
+        user.is_active = True
         user.save(using=self._db)
         return user
